6149879cab47be5980202ccf93e012c5acc18421
Patrik Maunus <subscription@skriptladan.se>
2021-05-24 16:48:49 +0200

    Add the old path to PATH
    I forgot append the new path to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,19 @@
-#!/usr/bin/env fish
+#!/usr/bin/fish
 
-if test -d $HOME/.local/bin
-    set -gx PATH $HOME/.local/bin
+if test -d "$HOME/.local/bin"
+    set -gx PATH $PATH $HOME/.local/bin
 end
 
-if test -d $HOME/.ghcup/bin
-    set -gx PATH $HOME/.ghcup/bin
+if test -d "$HOME/.ghcup/bin"
+    set -gx PATH $PATH $HOME/.ghcup/bin
 end
 
-if test -d $HOME/.cabal/bin
-    set -gx PATH $HOME/.cabal/bin
+if test -d "$HOME/.cabal/bin"
+    set -gx PATH $PATH $HOME/.cabal/bin
+end
+
+if test -x (which nvim)
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 end
 
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,4 @@
+function fish_prompt
+    eval $GOPATH/bin/powerline-go -error $status -newline -shell bare
+end
+
