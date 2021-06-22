ab70a2272bd9a1714088797d87462d02aac0f94e
Patrik Maunus <subscription@skriptladan.se>
2021-06-22 13:49:19 +0200

    Remove old path checks in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,6 @@
 #!/usr/bin/fish
 
-if test -d $HOME/.local/bin
-    set -gx PATH $PATH $HOME/.local/bin
-end
-
-if test -d $HOME/.ghcup/bin
-    set -gx PATH $PATH $HOME/.ghcup/bin
-end
-
-if test -d $HOME/.cabal/bin
-    set -gx PATH $PATH $HOME/.cabal/bin
-end
-
 if test -x (which nvim)
     set -gx EDITOR nvim
     set -gx VISUAL nvim
 end
-
